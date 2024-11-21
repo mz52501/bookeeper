dc27cf61971639e5b851aae21adc95b003687f81
MarkoZura <marko.zura@fer.hr>
2024-11-21 18:11:40 +0100

    adding project

---
+++ src/main/java/com/example/javaxml_bookeeper/JavaXmlBookeeperApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaxml_bookeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaXmlBookeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaXmlBookeeperApplication.class, args);
+    }
+
+}
